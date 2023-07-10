be8925ad95f8bdf24af5cbeb3aca89d30635e235
Joaquim Gregório da Silva Neto <joaquim.ibi@gmail.com>
2023-07-10 19:28:25 -0300

    refactor: comment parse and logTree functions

--- src/lib/tree.ts
+++ src/lib/tree.ts
@@ -4,40 +4,53 @@ interface TreeNode {
   children?: TreeNode[];
 }
 
+/**
+ * Parses an array of tokens and constructs a parser tree.
+ *
+ * @param {string[]} tokens - The array of tokens to parse.
+ * @returns {TreeNode} The root node of the parser tree.
+ */
 function parse(tokens: string[]): TreeNode {
   let index = 0;
 
   function walk(): TreeNode | undefined {
     const token = tokens[index];
 
+    // NOTE: check if token is an identifier
     if (token.startsWith('IDENTIFIER(')) {
       index++;
       return { type: 'identifier', value: token.slice(11, -1) };
     }
 
+    // NOTE: check if token is an integer
     if (token.startsWith('INTEGER(')) {
       index++;
       return { type: 'integer', value: parseInt(token.slice(8, -1)) };
     }
 
+    // NOTE: check if token is a string
     if (token.startsWith('STRING("') && token.endsWith('")')) {
       index++;
       return { type: 'string', value: token.slice(8, -2) };
     }
 
+    // NOTE: check if token is a keyword or operator
     if (token === 'LET' || token === 'ASSIGN' || token === 'PLUS' || token === 'MINUS' || token === 'MULTIPLY' || token === 'DIVIDE') {
       index++;
       return { type: token.toLowerCase() };
     }
 
+    // NOTE: check if token is a punctuation
     if (token === 'LEFT_PAREN' || token === 'RIGHT_PAREN' || token === 'COMMA' || token === 'COLON' || token === 'QUESTION_MARK' || token === 'PERIOD') {
       index++;
     }
 
+    // NOTE: check if token is the start of an array
     if (token === '[') {
       index++;
       const node: TreeNode = { type: 'array', children: [] };
 
+      // NOTE: process the elements inside the array
       while (tokens[index] !== ']') {
         const element = walk();
         if (element) {
@@ -45,7 +58,7 @@ function parse(tokens: string[]): TreeNode {
         }
       }
 
-      index++; // Consume ']'
+      index++; // consume ']'
       return node;
     }
 
@@ -54,6 +67,7 @@ function parse(tokens: string[]): TreeNode {
 
   const rootTreeNode: TreeNode = { type: 'program', children: [] };
 
+  // NOTE: process each statement in the token array
   while (index < tokens.length) {
     const statement = walk();
     if (statement) {
@@ -64,6 +78,13 @@ function parse(tokens: string[]): TreeNode {
   return rootTreeNode;
 }
 
+/**
+ * Logs the parser tree representation of the given tokens.
+ *
+ * @param {string[]} tokens - The array of tokens to generate the parser tree from.
+ * @param {string} [treeName] - An optional name for the parser tree. If provided, it will be logged before the tree.
+ * @returns {void}
+ */
 function logTree(tokens: string[], treeName?: string): void {
   treeName && console.log(treeName);
   const tree = parse(tokens);
